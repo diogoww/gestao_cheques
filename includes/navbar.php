80be43ad76c5132a0b2e98dc94e9d3e166c0bf3e
Diogo Varaschin <diogovaraschin@outlook.com>
2025-11-30 19:43:16 -0300

    feat: add 'Back' and 'Home' buttons with distinct styling

--- includes/navbar.php
+++ includes/navbar.php
@@ -6,12 +6,14 @@
 
 <nav class="navbar navbar-expand-lg bg-pastel navbar-light">
   <div class="container">
-    <a class="navbar-brand" href="gestao_cheques/index.php">Gestão Cheques</a>
+    <a class="navbar-brand">Cheque Master</a>
     <?php if (isset($_SESSION['user_id'])): ?>
       <div class="d-flex gap-2">
         <a class="btn btn-outline-dark" href="/gestao_cheques/cheques/list.php">Cheques</a>
         <a class="btn btn-outline-dark" href="/gestao_cheques/fornecedores/list.php">Fornecedores</a>
-        <a class="btn btn-danger" href="/gestao_cheques/auth/logout.php">Sair</a>
+        <a class="btn btn-voltar" href="javascript:history.back()">Voltar uma Página</a>
+        <a class="btn btn-success" href="/gestao_cheques/index.php">Voltar ao Início</a>
+        <a class="btn btn-outline-dark btn-sair" href="/auth/logout.php">Sair</a>
       </div>
     <?php endif; ?>
   </div>
