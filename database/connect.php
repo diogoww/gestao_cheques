b510406d26994c24a77cd0aa92e4fce4fa6ae101
Diogo Varaschin <diogovaraschin@outlook.com>
2025-11-30 13:31:38 -0300

    chore(database): up db connection to use port 3307

--- database/connect.php
+++ database/connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "gestao_cheques");
+$conn = new mysqli("localhost", "root", "", "gestao_cheques", 3307);
 
 if ($conn->connect_error) {
     die("Erro na conexão: " . $conn->connect_error);
