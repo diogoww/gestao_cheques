2e1be9a7d0162452e0b1e1d15c7675916904d608
Diogo Varaschin <diogovaraschin@outlook.com>
2025-11-30 12:29:39 -0300

    feat: configure the header

---
+++ includes/header.php
@@ -0,0 +1,5 @@
+<?php
+?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="/style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
