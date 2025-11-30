2d269d0dc9a0108f3fe66bc8d7442dcda10026b7
Diogo Varaschin <diogovaraschin@outlook.com>
2025-11-30 13:32:34 -0300

    fix(includes):  update file path in buttons

--- includes/navbar.php
+++ includes/navbar.php
@@ -6,12 +6,12 @@
 
 <nav class="navbar navbar-expand-lg bg-pastel navbar-light">
   <div class="container">
-    <a class="navbar-brand" href="/index.php">Gestão Cheques</a>
+    <a class="navbar-brand" href="gestao_cheques/index.php">Gestão Cheques</a>
     <?php if (isset($_SESSION['user_id'])): ?>
       <div class="d-flex gap-2">
-        <a class="btn btn-outline-dark" href="/cheques/list.php">Cheques</a>
-        <a class="btn btn-outline-dark" href="/fornecedores/list.php">Fornecedores</a>
-        <a class="btn btn-danger" href="/auth/logout.php">Sair</a>
+        <a class="btn btn-outline-dark" href="/gestao_cheques/cheques/list.php">Cheques</a>
+        <a class="btn btn-outline-dark" href="/gestao_cheques/fornecedores/list.php">Fornecedores</a>
+        <a class="btn btn-danger" href="/gestao_cheques/auth/logout.php">Sair</a>
       </div>
     <?php endif; ?>
   </div>
