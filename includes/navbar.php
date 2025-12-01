fbc2f9be204c7161acd74da6c7a2fdb461eb520a
Diogo Varaschin <diogovaraschin@outlook.com>
2025-11-30 22:01:27 -0300

    style(navbar): change 'Voltar ao Início' button to orange color scheme

--- includes/navbar.php
+++ includes/navbar.php
@@ -12,7 +12,7 @@
         <a class="btn btn-outline-dark" href="/gestao_cheques/cheques/list.php">Cheques</a>
         <a class="btn btn-outline-dark" href="/gestao_cheques/fornecedores/list.php">Fornecedores</a>
         <a class="btn btn-voltar" href="javascript:history.back()">Voltar uma Página</a>
-        <a class="btn btn-success" href="/gestao_cheques/index.php">Voltar ao Início</a>
+        <a class="btn btn-voltar" href="/gestao_cheques/index.php">Voltar ao Início</a>
         <a class="btn btn-outline-dark btn-sair" href="/gestao_cheques/auth/logout.php">Sair</a>
       </div>
     <?php endif; ?>
